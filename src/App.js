82d3fc12a6683d23092b0009c37d60420f347610
geekim1203 <swagchanghyuk1203@naver.com>
2022-04-27 19:02:03 +0900

    To do list
    to do list : useState, map

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
 import { useState, useEffect } from "react";
 
-function Hello() {
-  useEffect(() => {
-    console.log("Hi :)");
-    return () => console.log("bye :(");
-  }, []);
-  // useEffect(function () {
-  //   console.log("Hi :)");
-  //   return function () {
-  //     console.log("Bye ):");
-  //   };
-  // }, []);
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => {
-    setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChagne = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    // currentArray는 처음엔 비어있고 todolist를 추가 하면 생긴다. => 이후에 대한 설명으로는 todo는 input에 입력하는 새로운 todo value이고 ...currentArray는 이전에 추가했던 todo list 즉
+    // => todo, ...currentArray는 새로운toDo와 이전의 todo를 더해서 listUp 하는 코드
+    //만약 todo가 4 currentArray가 1 2 3 이었을때 ...currentArray에서 ...을 뺀다면 / [4, [1 2 3]] / 이렇게 됨 ...을 넣으면 /[4, 1 2 3 ] 이렇게 되는것
   };
+
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChagne}
+          type="text"
+          placeholder="Write yout to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+        {/* map은 하나의 array에 있는 item을 내가 원하는 무엇이든지로 바꿔주고 새로운 array로 return 한다. / 예전 array를 가져와서 변형시킴  */}
+      </ul>
     </div>
   );
 }
