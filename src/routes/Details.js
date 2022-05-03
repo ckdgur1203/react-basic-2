70a066b04555860072bd76a52be1a833320b95ff
geekim1203 <swagchanghyuk1203@naver.com>
2022-05-03 16:52:52 +0900

    movies
    error = "map is not a function"

--- src/routes/Details.js
+++ src/routes/Details.js
@@ -1,22 +1,24 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useCallback } from "react";
 
 import Movie from "../components/Movie";
 function Details() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const { id } = useParams();
-  const getMovie = async () => {
+  const getMovie = useCallback(async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    setMovies(json.data.movies);
+    setMovies(json.data.movie);
     setLoading(false);
-  };
+    console.log(json.data.movie);
+  }, [id]);
   useEffect(() => {
     getMovie();
-  }, []);
-  console.log(movies);
+  }, [getMovie]);
+
   return (
     <div>
       <div>
@@ -24,7 +26,16 @@ function Details() {
           <h1>Loading...</h1>
         ) : (
           <div>
-            <Movie />
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                coverImg={movie.medium_cover_image}
+                title={movie.title}
+                summary={movie.summary}
+                genres={movie.genres}
+              />
+            ))}
           </div>
         )}
       </div>
